77dbbcbd2abd4518cdd4d01b1bafd3264ff69a6d
yashmita-k <yashmita.ranjeet@gmail.com>
2026-06-19 11:03:47 +0530

    Add files via upload

---
+++ components/MatchScoreCard.jsx
@@ -0,0 +1,16 @@
+function MatchScoreCard() {
+  return (
+    <div className="border p-4 rounded">
+      <h1>91</h1>
+
+      <p>Strong Match</p>
+
+      <ul>
+        <li>Audience Alignment</li>
+        <li>Relevant Content Style</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MatchScoreCard;
